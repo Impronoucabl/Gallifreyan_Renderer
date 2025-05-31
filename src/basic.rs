6b33eaf978c0f8a3eb240a71fb3a02133ca85679
Impronoucabl <Impronoucabl@gmail.com>
2025-05-31 17:57:31 +1000

    Basic arcs done

--- src/basic.rs
+++ src/basic.rs
@@ -1,15 +1,37 @@
+use svg::node::element::path::{Command, Data};
 use svg::Document;
-use svg::node::element::{Circle};
+use svg::node::element::{Circle, Path};
 
 use crate::ctx as context;
 
 pub fn circle(doc:Document, center:(f64,f64), radius:f64, ctx:&context::Context) -> Document {
+    let center = (center.0 + ctx.origin().0,-center.1 + ctx.origin().1);
     let circle = Circle::new()
         .set("fill", ctx.colour().fill())
         .set("stroke", ctx.colour().stroke())
         .set("stroke-width", ctx.stroke().strokewidth())
-        .set("cx", center.0 + ctx.origin().0)
-        .set("cy", -center.1 + ctx.origin().1)
+        .set("cx", center.0)
+        .set("cy", center.1)
         .set("r", radius);
     doc.add(circle)
+}
+
+pub fn arc_circle(doc:Document,start:(f64,f64), end:(f64,f64),radius:f64, ctx:&context::Context) -> Document {
+    let start = (start.0 + ctx.origin().0,ctx.origin().1 - start.1);
+    let end = (end.0 + ctx.origin().0,ctx.origin().1 - end.1);
+    let data = Data::new()
+        .move_to(start)
+        .elliptical_arc_to((
+            radius,radius,
+            0.0, //angle offset
+            0.0, //large arc
+            1.0, //sweep dir
+            end.0,end.1,
+        ));
+    let arc = Path::new()
+        .set("d", data)
+        .set("fill", ctx.colour().fill())
+        .set("stroke", ctx.colour().stroke())
+        .set("stroke-width", ctx.stroke().strokewidth());
+    doc.add(arc)
 }
--- src/instructions.rs
+++ src/instructions.rs
@@ -4,13 +4,21 @@ use crate::pord;
 use crate::basic;
 use crate::ctx;
 
-pub fn do_this(mut doc:svg::Document, origin:(f64,f64)) -> svg::Document {
+fn prelude(origin:(f64,f64)) -> (ctx::Context,ctx::Context,ctx::Context,ctx::Context) {
     let colour = ctx::ColourContext::new("none","black");
     let stroke = ctx::StrokeContext::new(20.0);
-    let prime_ctx = ctx::Context::new(colour,stroke, origin);        
-    let doc2 = basic::circle(doc, (0.0,0.0), 1000.0,&prime_ctx);
-    doc = basic::circle(doc2, (0.0,800.0), 100.0,&prime_ctx);
-    let thin_ctx = prime_ctx.new_strokewidth(10.0);
-    doc = basic::circle(doc, (500.0,0.0), 250.0, &thin_ctx);
+    let prime_ctx = ctx::Context::new(colour,stroke,origin);
+    let word_ctx = prime_ctx.new_strokewidth(10.0); 
+    let lett_ctx =  prime_ctx.new_strokewidth(8.0); 
+    let lett2_ctx =  prime_ctx.new_strokewidth(10.0);
+    (prime_ctx,word_ctx,lett_ctx,lett2_ctx)
+}
+
+pub fn do_this(mut doc:svg::Document, origin:(f64,f64)) -> svg::Document {
+    let (prime_ctx,word_ctx,lett_ctx,lett2_ctx) = prelude(origin);
+    doc = basic::circle(doc, (0.0,0.0), 1000.0,&prime_ctx);
+    doc = basic::circle(doc, (0.0,800.0), 100.0,&prime_ctx);
+    doc = basic::arc_circle(doc, (-400.0,-300.0),(0.0,500.0),500.0,&lett_ctx);
+    doc = basic::circle(doc, (300.0,0.0), 250.0, &word_ctx);
     basic::circle(doc, (0.0,0.0),300.0, &prime_ctx)
 }
